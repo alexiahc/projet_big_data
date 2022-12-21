2698a0e6e1efee879a7e1771a8be564d73af345d
alexiahc <103626666+alexiahc@users.noreply.github.com>
2022-12-21 16:00:27 +0100

    Update projet.scala

--- projet.scala
+++ projet.scala
@@ -42,13 +42,7 @@ object MyApp {
         // split train test data ; target ArrDelay 
 
         var Array(x_train, x_test) = data.randomSplit(Array[Double](0.8, 0.2))
-        var y_train = x_train.select("ArrDelay")
-        var y_test = x_test.select("ArrDelay")
-        x_train = x_train.drop("ArrDelay")
-        x_test = x_test.drop("ArrDelay")
-        x_train = x_train.toDF()
-        x_test = x_test.toDF()
-
+        
         // exploring data
         x_train.dtypes
         // modifying the columns with object type 
@@ -71,36 +65,47 @@ object MyApp {
         for (c<-col_obj) {
             var d = x_train.groupBy(c).count()
             val nb_rows = d.count()
+
+            // for the columns with more then 9 different 
+            // possible values, we modify the column to keep 
+            // only 10 different values 
             if (nb_rows > 9) {
                 d = d.sort(col("count").desc)
                 var values_util = d.select(c).map(f=>f.getString(0)).collect.toList
                 values_util = values_util.take(9)
                 // remplace by "other" the values that aren't 
-                // in the list values_util
+                // in the list values_util, which are the 9 values 
+                // the most frequent in the dataset 
                 x_train = x_train.withColumn(c, when(col(c).isin(values_util:_*), col(c)).otherwise("Other"))
                 x_test= x_test.withColumn(c, when(col(c).isin(values_util:_*), col(c)).otherwise("Other")) 
             }
         } 
-        // for the columns with more then 10 different 
-        // possible values, we modify the column to keep 
-        // only 10 different values 
-
+        
         // the cancellation code and the fligh number will not be usefull for the model
         x_train = x_train.drop("CancellationCode", "FlightNum")
 
-        // if the value in the column DepTime is null, it means that the 
-        // fligh never start so the value in the column ArrDelay is also null
-        // we keep just the values that are interesting for us for a model, 
-        // with not-null values for these two columns 
-
+        // If the value in the column DepTime is null, it means that the 
+        // fligh never started so the value in the column ArrDelay is also null
+        // We keep just the values that are interesting for us for a prediction model, 
+        // with not-null values for these two columns, in removing the rows 
+        // where ArrayDelay is null 
+        x_train.filter(!col("ArrDelay").isNull).show()
 
         // using the one-hot encoder to create new columns for categorical values 
 
         // remplacing the null values 
 
+        // removing the target column from the predictors 
+        var y_train = x_train.select("ArrDelay")
+        var y_test = x_test.select("ArrDelay")
+        x_train = x_train.drop("ArrDelay")
+        x_test = x_test.drop("ArrDelay")
+        x_train = x_train.toDF()
+        x_test = x_test.toDF()
 
         // using algorithms to keep just the necessary columns for the model 
 
+        
     }
 
     def test_model() {
